8b28ce16a022bbbcca5b0f85d937d452a2aeddce
odonML <odon_ml@outlook.com>
2021-12-09 20:41:53 -0600

    refactor navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import "./Navbar.css";
 function Navbar() {
     const [active, setActive] = useState(0);
-    const activeOne = `item ${active === 1 ? "active" : "inactive"}`;
-    const activeTwo = `item ${active === 2 ? "active" : "inactive"}`;
-    const activeThree = `item ${active === 3 ? "active" : "inactive"}`;
+    
+    const handleClick = (item) => setActive(item);
+    const activeItem = (item) => `item ${active === item ? "active" : "inactive"}`
 
     return (
         <ul>
-            <li className={activeOne} onClick={() => setActive(1)}>
+            <li className={activeItem(1)} onClick={() => handleClick(1) }>
                 item1
             </li>
-            <li className={activeTwo} onClick={() => setActive(2)}>
-                item1
+            <li className={activeItem(2)} onClick={() => handleClick(2)}>
+                item2
             </li>
-            <li className={activeThree} onClick={() => setActive(3)}>
-                item1
+            <li className={activeItem(3)} onClick={() => handleClick(3)}>
+                item3
             </li>
         </ul>
     );
